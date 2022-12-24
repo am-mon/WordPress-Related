e951170d32d86b6cbe3966b984753b2c4faa8789
Aye Myat Mon <ayemyatmon.yn@gmail.com>
2022-12-24 20:40:32 +0800

    Create Disable updates via wp-config.php

---
+++ Disable updates via wp-config.php
@@ -0,0 +1,7 @@
+//Disable updates
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+
+//External block for faster loading
+define( 'WP_HTTP_BLOCK_EXTERNAL', true );
+define('WP_ACCESSIBLE_HOSTS', '*.imagify.io,*.wp-rocket.me,*.ipinfo.io,*.google.com,*.bing.com,*.googleapis.com,*.updraftplus.com,*.googleusercontent.com,*.wordfence.com,*.amazon.com,*.amazonaws.com,*.cloudfront.net, *.paypal.com, .wpmudev.org, .elementor.com');
